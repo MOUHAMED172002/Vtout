677cbbcae3833496fc7cb37d89809566a94fe04e
MOUHAMED172002 <abdoul172002@gmail.com>
2026-05-12 03:17:40 +0100

    fix: wrap app in BrowserRouter at the root level

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 import { Providers } from './Providers'
@@ -8,9 +9,11 @@ import ErrorBoundary from './component/Shared/ErrorBoundary.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ErrorBoundary>
-      <Providers>
-        <App />
-      </Providers>
+      <BrowserRouter>
+        <Providers>
+          <App />
+        </Providers>
+      </BrowserRouter>
     </ErrorBoundary>
   </React.StrictMode>
 )
